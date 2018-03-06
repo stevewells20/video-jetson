66d15762f0dd14686254c92bbd71c10986c23705
Steven Wells <stevewells20@gmail.com>
2018-03-05 20:55:23 -0800

    Final commit for jetson version

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <gst/gst.h>
 #include <string>
-#include <iostream>
 #include <cstdlib>
 
 int main (int argc, char *argv[])
@@ -12,30 +11,18 @@ int main (int argc, char *argv[])
 
   gst_init (&argc, &argv);
 
-  std::string host = "10.0.0.26";
-  std::string port = "5000";
-  // for (int x=0;x < argc; x++) {
-  //   std::cout << x << " : " << argv[x] << std::endl;
+  // if (argc != 2) {
+  //   g_print ("usage: %s <filename>\n", argv[0]);
+  //   return -1;
   // }
-
-  if (argc > 1) {
-    host = argv[1];
-  }
-  if (argc > 2) {
-    port = argv[2];
-  }
     /* Build the pipeline */
-    g_print("Building pipeline:\n"
-            "\tHost = %s\n"
-            "\tPort = %s\n",
-            host.c_str(), port.c_str());
     std::string pipeString = "v4l2src device=/dev/video0 ! "
           "video/x-raw, format=(string)I420, width=640,height=480, framerate=40/1 ! "
           "nvvidconv ! "
           "video/x-raw(memory:NVMM), format=(string)I420 ! "
           "omxh264enc low-latency=true ! "
           "rtph264pay config-interval=3 ! "
-          "udpsink sync=false host=" + host + " port=" + port;
+          "udpsink sync=false host=10.0.0.26 port=5000";
     pipeline = gst_parse_launch (pipeString.c_str(), &error);
     if (!pipeline) {
       g_print ("Parse error: %s\n", error->message);
